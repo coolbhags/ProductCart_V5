213dae00503cc0cdb4980ab2b916fefb828b60a9
Joshi <bhagyesh.h.joshi@accenture.com>
2018-06-11 17:44:20 +0530

    V5

---
+++ src/app/product-list/produc.ts
@@ -0,0 +1,12 @@
+export interface IProduct
+{
+    productId: number;
+    productName:string;
+    productCode:string;
+
+    releaseate:string;
+    price:number;
+    description:string;
+    starRating: number;
+    imageUrl:string;
+}
